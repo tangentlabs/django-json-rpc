fec6b800aa487c2fbe86b84258ed0abe82c42a3a
Nathan Humphreys <nathan.humphreys@tangentsnowball.com>
2014-09-19 11:48:30 +0100

    Use body instead of raw_post_data
    For django 1.6 support we should use the ```body``` attribute and not
    the ```raw_post_data``` attribute as the later has been removed

--- jsonrpc/site.py
+++ jsonrpc/site.py
@@ -56,7 +56,7 @@ def validate_params(method, D):
       raise InvalidParamsError('Not enough params provided for %s' % method.json_sig)
     for k in keys:
       if not k in D['params']:
-        raise InvalidParamsError('%s is not a valid parameter for %s' 
+        raise InvalidParamsError('%s is not a valid parameter for %s'
                                  % (k, method.json_sig))
       if not Any.kind(D['params'][k]) == method.json_arg_types[k]:
         raise InvalidParamsError('%s is not the correct type %s for %s'
@@ -90,7 +90,7 @@ class JSONRPCSite(object):
 
   def register(self, name, method):
     self.urls[unicode(name)] = method
-  
+
   def empty_response(self, version='1.0'):
     resp = {'id': None}
     if version == '1.1':
@@ -100,7 +100,7 @@ class JSONRPCSite(object):
       resp['jsonrpc'] = version
     resp.update({'error': None, 'result': None})
     return resp
-  
+
   def validate_get(self, request, method):
     encode_get_params = lambda r: dict([(k, v[0] if len(v) == 1 else v)
                                          for k, v in r])
@@ -115,7 +115,7 @@ class JSONRPCSite(object):
         }
         return True, D
     return False, {}
-  
+
   def response_dict(self, request, D, is_batch=False, version_hint='1.0', json_encoder=None):
     json_encoder = json_encoder or self.json_encoder
     version = version_hint
@@ -123,9 +123,9 @@ class JSONRPCSite(object):
     apply_version = {'2.0': lambda f, r, p: f(r, **encode_kw(p)) if type(p) is dict else f(r, *p),
                      '1.1': lambda f, r, p: f(r, *encode_arg11(p), **encode_kw(encode_kw11(p))),
                      '1.0': lambda f, r, p: f(r, *p)}
-    
+
     try:
-      # params: An Array or Object, that holds the actual parameter values 
+      # params: An Array or Object, that holds the actual parameter values
       # for the invocation of the procedure. Can be omitted if empty.
       if 'params' not in D:
          D['params'] = []
@@ -134,7 +134,7 @@ class JSONRPCSite(object):
       if D['method'] not in self.urls:
         raise MethodNotFoundError('Method not found. Available methods: %s' % (
                         '\n'.join(self.urls.keys())))
-      
+
       if 'jsonrpc' in D:
         if str(D['jsonrpc']) not in apply_version:
           raise InvalidRequestError('JSON-RPC version %s not supported.' % D['jsonrpc'])
@@ -145,7 +145,7 @@ class JSONRPCSite(object):
         version = request.jsonrpc_version = response['version'] = str(D['version'])
       else:
         request.jsonrpc_version = '1.0'
-        
+
       method = self.urls[str(D['method'])]
       if getattr(method, 'json_validate', False):
         validate_params(method, D)
@@ -173,7 +173,7 @@ class JSONRPCSite(object):
       response['result'] = R
 
       status = 200
-    
+
     except Error, e:
       signals.got_request_exception.send(sender=self.__class__, request=request)
       response['error'] = e.json_rpc_format
@@ -193,9 +193,9 @@ class JSONRPCSite(object):
     # allowed to specify both or none.
     if version in ('1.1', '2.0') and 'error' in response and not response['error']:
       response.pop('error')
-    
+
     return response, status
-  
+
   @csrf_exempt
   def dispatch(self, request, method='', json_encoder=None):
     from django.http import HttpResponse
@@ -216,10 +216,10 @@ class JSONRPCSite(object):
           if hasattr(request, "body"):
               D = loads(request.body)
           else:
-              D = loads(request.raw_post_data)
+              D = loads(request.body)
         except:
           raise InvalidRequestError
-      
+
       if type(D) is list:
         response = [self.response_dict(request, d, is_batch=True, json_encoder=json_encoder)[0] for d in D]
         status = 200
@@ -227,7 +227,7 @@ class JSONRPCSite(object):
         response, status = self.response_dict(request, D, json_encoder=json_encoder)
         if response is None and (not u'id' in D or D[u'id'] is None): # a notification
           return HttpResponse('', status=status)
-      
+
       json_rpc = dumps(response, cls=json_encoder)
     except Error, e:
       signals.got_request_exception.send(sender=self.__class__, request=request)
@@ -240,22 +240,22 @@ class JSONRPCSite(object):
       other_error = OtherError(e)
       response['result'] = None
       response['error'] = other_error.json_rpc_format
-      status = other_error.status    
-      
+      status = other_error.status
+
       json_rpc = dumps(response,cls=json_encoder)
-    
+
     return HttpResponse(json_rpc, status=status, content_type='application/json-rpc')
-  
+
   def procedure_desc(self, key):
     M = self.urls[key]
     return {
       'name': M.json_method,
       'summary': M.__doc__,
       'idempotent': M.json_safe,
-      'params': [{'type': str(Any.kind(t)), 'name': k} 
+      'params': [{'type': str(Any.kind(t)), 'name': k}
         for k, t in M.json_arg_types.iteritems()],
       'return': {'type': M.json_return_type}}
-  
+
   def service_desc(self):
     return {
       'sdversion': '1.0',
@@ -263,10 +263,10 @@ class JSONRPCSite(object):
       'id': 'urn:uuid:%s' % str(self.uuid),
       'summary': self.__doc__,
       'version': self.version,
-      'procs': [self.procedure_desc(k) 
+      'procs': [self.procedure_desc(k)
         for k in self.urls.iterkeys()
           if self.urls[k] != self.describe]}
-  
+
   def describe(self, request):
     return self.service_desc()
 
